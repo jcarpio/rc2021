451745ffe1cbae7471b4e763a7c907965453fe88
Jose Carpio <jose.carpio@gmail.com>
2021-03-18 14:22:27 +0100

    cuarta sesion

--- reverse.pl
+++ reverse.pl
@@ -1,5 +1,10 @@
 
-% reverse(+List, -ListR) que es cierto cuando
+% reverse(?List, ?ListR) que es cierto cuando
 % ListR unifica con una lista que contiene 
 % los mismos elementos que List pero en 
 % orden inverso.
+
+reverse([], []).
+
+reverse([Cab|Resto], R2):- reverse(Resto, R),
+  append(R, [Cab], R2).
