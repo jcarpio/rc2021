769a5b20a40cb3162dc5e9c6829a3d53dcdcf5a2
Jose Carpio <jose.carpio@gmail.com>
2021-06-10 17:20:12 +0200

    ultima sesion

--- exaamen_junio_2019.pl
+++ exaamen_junio_2019.pl
@@ -47,8 +47,7 @@ mas_veces(Lista, E, N):-
   msort(Lista, L2),
   comprime(L2, R),
   tupla_n_mayor(R, E, N).
-    
-  
+     
 % tupla_n_mayor(Lista, E, N)
 % es cierto si E unifica con el elemento de lista que
 % se repite más veces y N el número de veces que se repite,
@@ -65,6 +64,111 @@ tupla_n_mayor([(Cab,N)|Resto], Cab, N):-
 tupla_n_mayor([(Cab,N)]|Resto], Emayor, Nmayor):-
   tupla_n_mayor(Resto, Emayor, Nmayor),
   N =< Nmayor.
+  
+/*
+(1.5 puntos) Implemente el predicado lista_hojas(+Arbol_binario, +Lista_hojas) que
+es cierto cuando Lista_hojas unifica con las lista de etiquetas de las hojas contenidas en el árbol
+binario
+*/
+
+lista_hojas(nil, []).
+
+hoja(a(_, nil, nil)).
+
+lista_hojas(a(E,nil,nil), [E]).
+
+lista_hojas(a(_, Hi, Hd), R):-
+  \+ hoja(a(_,Hi,Hd)),
+  lista_hojas(Hi, Ri),
+  lista_hojas(Hd, Rd),
+  append(Ri, Rd, R).
+  
+  
+/*
+(1.5 puntos) Implemente el predicado balanceado(+Arbol_binario) 
+que es cierto cuando la diferencia entre la altura del arbol 
+derecho y el izquierdo de cualquier nodo es como máximo 1.
+
+      1 
+     / \
+    2   5
+   /     \
+  3       6
+ /	       \
+4           7
+
+*/
+
+balanceado(nil).
+
+balanceado(a(_, Hi, Hd)):-
+  altura(Hi, Ai),
+  altura(Hd, Ad),
+  X is abs(Hi - Hd),
+  X =< 1,
+  balanceado(Hi),
+  balanceado(Hd).
+ 
+altura(nil, 0).
+altura(a(_, Hi, Hd), R):-
+  altura(Hi, Ai),
+  altura(Hd, Ad),
+  X is max(Ai, Ad),
+  R is X + 1.
+  
+/*
+(1.5 puntos) Implemente el predicado 
+construye_arbol_generico(+Lista, -Arbol_generico) que es cierto 
+Arbol_generico unifica con un árbol genérico que contiene
+todas las etiquetas de Lista.
+*/
+
+construye_arbol_generico([Cab|Resto], a(Cab, R)):-
+  construye_lista_arboles_genericos(Resto, R).
+
+construye_lista_arboles_genericos([], []).
+construye_lista_arboles_genericos([Cab|Resto], [a(Cab,[])|R]):-
+   construye_lista_arboles_genericos(Resto, R).
+   
+   
+/*
+(2 puntos) Implemente el predicado ciclos(+Grafo, -Lista_ciclos) que es cierto cuando
+Lista_ciclos unifica con todos los ciclos de Grafo.   
+
+Supongo que el grafo es dirigido, el camino es una
+lista de vértices y los visitados es una lista de
+aristas.
+
+Llamaré al predicado que devuelve todas las soluciones
+bolsa(Plantilla, Objetivo, Lista)
+
+*/
+
+camino(_, Ini,Ini,_,[Ini]).
+camino(grafo(V,A), Ini, Fin, Visitados, [Ini|Camino]):-
+  conectado(Ini, Tmp, A),
+  \+ member(arista(Ini,Tmp), Visitados),
+  camino(grafo(V,A), Tmp, Fin, [arista(Ini,Tmp)|Visitados], 
+  Camino).
+  
+ciclo(grafo(V,A), Ciclo):- member(Vertice, V),
+       camino(grafo(V,A), Vertice, Vertice, [], Ciclo), 
+	   length(Ciclo, L), L > 1.
+	   
+ciclos(G, R):- bagof(C, ciclo(G, C), R).
+
+/*
+iguales(A1, A2),
+es cierto si los árboles binario/generico A1 y A2 son iguales.
+*/
+
+iguales(A,A).
+  
+ 
+
+  
+  
+
   
   
 
